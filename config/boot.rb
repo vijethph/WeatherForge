4ad9d5673e40c09248c33546b628f3bd57350877
vijethph <vijeth.ph@outlook.com>
2026-01-05 17:36:50 +0000

    fix(ci): update tests - 2

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,6 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# Load PostGIS adapter early to enable geometry column type in schema.rb
+require "active_record/connection_adapters/postgis_adapter" if defined?(ActiveRecord)
